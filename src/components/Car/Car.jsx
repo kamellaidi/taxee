0834b06621ab34c8807276d24e121a121dc4753b
kamellaidi <kamellaidi@outlook.fr>
2023-07-03 22:42:01 +0200

    ajout categories et prec et suivant

--- src/components/Car/Car.jsx
+++ src/components/Car/Car.jsx
@@ -3,20 +3,27 @@ import carimg from '../../assets/car.jpeg';
 import './car.css';
 
 const Car = ({ taxi }) => {
+  // Extraction des propriétés de l'objet taxi
   const { vehicle_type, driver, category, engine_type, seats } = taxi;
+  
   return (
     <div className='cars-inner-box'>
       <h2>{vehicle_type}</h2>
       <img className='cars-img' src={carimg} alt={vehicle_type} />
       <p>
+        {/* Affichage de la catégorie */}
         Catégorie : {category}
         <br />
+        {/* Affichage du type de moteur */}
         Type de moteur : {engine_type}
         <br />
+        {/* Affichage du nombre de places */}
         Nombre de places disponibles : {seats}
         <br />
+        {/* Affichage du chauffeur */}
         Chauffeur : {driver.name}
         <br />
+        {/* Affichage de la description du chauffeur */}
         Description du chauffeur : {driver.description}
       </p>
     </div>
--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -1,41 +1,89 @@
 import React from 'react';
-import './cars.css'
-import Car from '../Car/Car' 
-import taxis from '../data/taxisdriver'
+import { useState } from 'react';
+import './cars.css';
+import Car from '../Car/Car';
+import taxis from '../data/taxisdriver';
 
 const Cars = () => {
+  // Gestion des états
+  const [startIndex, setStartIndex] = useState(0); // Indice de départ pour l'affichage des taxis
+  const [selectedCategory, setSelectedCategory] = useState(''); // Catégorie sélectionnée pour le filtrage
+
+  // Fonction pour passer à la page suivante
+  const handleNext = () => {
+    const nextIndex = startIndex + 2;
+    if (nextIndex < taxis.length) {
+      setStartIndex(nextIndex);
+    }
+  };
+
+  // Fonction pour revenir à la page précédente
+  const handlePrevious = () => {
+    const previousIndex = startIndex - 2;
+    if (previousIndex >= 0) {
+      setStartIndex(previousIndex);
+    }
+  };
+
+  // Fonction pour filtrer par catégorie
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setStartIndex(0);
+  };
+
+  // Fonction pour effacer le filtre et afficher tous les taxis
+  const handleClearFilter = () => {
+    setSelectedCategory('');
+    setStartIndex(0);
+  };
+
+  // Filtrage des taxis en fonction de la catégorie sélectionnée
+  const filteredTaxis = selectedCategory
+    ? taxis.filter((taxi) => taxi.category === selectedCategory)
+    : taxis;
+
+  // Affichage des taxis à partir de l'indice de départ
+  const displayedTaxis = filteredTaxis.slice(startIndex, startIndex + 2);
+
+  // Obtention de toutes les catégories disponibles
+  const categories = [...new Set(taxis.map((taxi) => taxi.category))];
+
   return (
     <div className='cars'>
       <h1 className='cars-title'>Nos véhicules</h1>
+      
+      {/* Boutons de navigation */}
+      <div className='navigation-buttons'>
+        <button onClick={handlePrevious} disabled={startIndex === 0}>
+          Précédent
+        </button>
+        <button onClick={handleNext} disabled={startIndex + 2 >= filteredTaxis.length}>
+          Suivant
+        </button>
+      </div>
+      
+      {/* Boutons de catégories */}
+      <div className='categories-buttons'>
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => handleCategoryClick(category)}
+            className={selectedCategory === category ? 'active' : ''}
+          >
+            {category}
+          </button>
+        ))}
+        <button onClick={handleClearFilter}>Tout</button>
+      </div>
+      
+      {/* Affichage des taxis */}
       <div className='cars-box'>
-      {console.log(taxis)}
-      {taxis.map((taxi) => (
+        {displayedTaxis.map((taxi) => (
           <Car key={taxi.id} taxi={taxi} />
         ))}
       </div>
     </div>
   );
 };
-
-// {/* <div className='cars-inner-box'>
-// <h2>Contenu de la boîte</h2>
-// <p>Ceci est le contenu de la deuxième boîte.</p>
-// </div>
-// <div className='cars-inner-box'>
-// <h2>Contenu de la boîte</h2>
-// <p>Ceci est le contenu de la première boîte.</p>
-// </div>
-// <div className='cars-inner-box'>
-// <h2>Contenu de la boîte</h2>
-// <p>Ceci est le contenu de la première boîte.</p>
-// </div>
-// <div className='cars-inner-box'>
-// <h2>Contenu de la boîte</h2>
-// <p>Ceci est le contenu de la première boîte.</p>
-// </div>
-// <div className='cars-inner-box'>
-// <h2>Contenu de la boîte</h2>
-// <p>Ceci est le contenu de la première boîte.</p>
-// </div> */}
 
 export default Cars;
